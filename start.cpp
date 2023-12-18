d4e64572b992aded711433485c71fd428e0ec739
Mark Morrison <mmorrisonlk@gmail.com>
2023-12-18 15:33:49 -0600

    It can make a hit roll

--- start.cpp
+++ start.cpp
@@ -1,40 +1,67 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
 int main ()
 {
-  int attackLevel, effectiveAttackLevel, attackRollMax, combatStyleBoost, equipmentAttackBonus, monsterDefenceLevel, defenceRollMax;
+  srand( (unsigned)time(NULL) );
+
+  float attackLevel, effectiveAttackLevel, attackRollMax, combatStyleBoost, equipmentAttackBonus, monsterDefenceLevel, defenceRollMax, hitChance, hitChanceFixed, hitRoll, damage, monsterHealth;
   signed monsterDefenceStyleBonus;
   bool attackFavored;
 
   attackLevel = 1;
   combatStyleBoost = 3;
   equipmentAttackBonus = 5;
+  damage = 1;
+  monsterHealth = 5;
 
   effectiveAttackLevel = attackLevel + combatStyleBoost + 8;
   attackRollMax = effectiveAttackLevel * (equipmentAttackBonus + 64);
-  // std::cout << attackRollMax;
 
   monsterDefenceLevel = 1;
   monsterDefenceStyleBonus = -15;
   defenceRollMax = (monsterDefenceLevel + 9) * (monsterDefenceStyleBonus + 64);
-  // std::cout << defenceRollMax;
 
   if (attackRollMax > defenceRollMax)
   {
     attackFavored = true;
   }
-  else {
+  else
+  {
     attackFavored = false;
   }
-  std::cout << attackFavored;
   
+  if (attackFavored == true)
+  {
+    hitChance = (1 - ((defenceRollMax + 2)/(2 * (attackRollMax + 1))));
+  }
+  else
+  {
+    hitChance = (attackRollMax / (2 * (defenceRollMax + 1)));
+  }
 
-  // while (enemyHealth > 0)
-  // {
-  //   enemyHealth = enemyHealth - attack;
-  //     std::cout << enemyHealth;
-  // }
+  hitChanceFixed = 32767 * hitChance;
+  std::cout << hitChanceFixed;
+  std::cout << "\n";
+
+  while (monsterHealth > 0)
+  {
+    hitRoll = rand();
+    std::cout << hitRoll;
+    std::cout << "\n";
+    if (hitRoll < hitChanceFixed)
+    {
+      monsterHealth = monsterHealth - damage;
+      std::cout << monsterHealth;
+      std::cout << "\n";
+    }
+    else
+    {
+      std::cout << "Miss \n";
+    }
+  }
 
   // terminate the program:
   return 0;
