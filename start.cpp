7876a94a836a5b775531445ac8c66d07e8c5ee0e
Mark Morrison <mmorrisonlk@gmail.com>
2023-12-18 13:52:32 -0600

    C can do math more news at 11

--- start.cpp
+++ start.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
-int main()
+using namespace std;
+
+int main ()
 {
-  std::cout << "Hello, World!" << std::endl;
+  int attackLevel, effectiveAttackLevel, attackRollMax, combatStyleBoost, equipmentAttackBonus, monsterDefenceLevel, defenceRollMax;
+  signed monsterDefenceStyleBonus;
+  bool attackFavored;
+
+  attackLevel = 1;
+  combatStyleBoost = 3;
+  equipmentAttackBonus = 5;
+
+  effectiveAttackLevel = attackLevel + combatStyleBoost + 8;
+  attackRollMax = effectiveAttackLevel * (equipmentAttackBonus + 64);
+  // std::cout << attackRollMax;
+
+  monsterDefenceLevel = 1;
+  monsterDefenceStyleBonus = -15;
+  defenceRollMax = (monsterDefenceLevel + 9) * (monsterDefenceStyleBonus + 64);
+  // std::cout << defenceRollMax;
+
+  if (attackRollMax > defenceRollMax)
+  {
+    attackFavored = true;
+  }
+  else {
+    attackFavored = false;
+  }
+  std::cout << attackFavored;
+  
+
+  // while (enemyHealth > 0)
+  // {
+  //   enemyHealth = enemyHealth - attack;
+  //     std::cout << enemyHealth;
+  // }
+
+  // terminate the program:
   return 0;
 }
