5aa18fb7fce9bd87927682d9df5b151f25586d62
Mark Morrison <mmorrisonlk@gmail.com>
2023-12-21 22:57:55 -0600

    It seems to be working with Rune Dragons

--- maxFight.cpp
+++ maxFight.cpp
@@ -7,37 +7,43 @@ using namespace std;
 
 int main ()
 {
-  srand( (unsigned)time(NULL) );
+    srand( (unsigned)time(NULL) );
 
-  float attackLevel, effectiveAttackLevel, attackRollMax, combatStyleAttackBoost, equipmentAttackBonus, hitChance, hitChanceFixed, hitRoll;
-  float strengthLevel, effectiveStrengthLevel, combatStyleStrengthBoost, maximumDamageHit, equipmentStrengthBonus, damageRoll, damage;
-  float monsterDefenceLevel, defenceRollMax, monsterHealth;
-  float swingTimer, battleTime, fights;
-  signed monsterDefenceStyleBonus;
-  bool attackFavored;
+    float attackLevel, effectiveAttackLevel, attackRollMax, combatStyleAttackBoost, equipmentAttackBonus, hitChance, hitChanceFixed, hitRoll;
+    float strengthLevel, effectiveStrengthLevel, combatStyleStrengthBoost, maximumDamageHit, equipmentStrengthBonus, damageRoll, damage;
+    float monsterDefenceLevel, defenceRollMax, monsterHealth;
+    float swingTimer, battleTime, totalBattleTime, averageTime, fights, superCombatBoost, prayerAttackBonus, prayerStrengthBonus;
+    signed monsterDefenceStyleBonus;
+    bool attackFavored;
 
-  attackLevel = 1;
-  strengthLevel = 1;
-  combatStyleAttackBoost = 3;
-  combatStyleStrengthBoost = 3;
-  equipmentAttackBonus = 5;
-  equipmentStrengthBonus = 7;
-  swingTimer = 2.4;
-  battleTime = 0;
-  fights = 0;
-  damage = 1;
-  monsterHealth = 5;
+    attackLevel = 99;
+    strengthLevel = 99;
+    superCombatBoost = 19;
+    prayerAttackBonus = 1.2;
+    prayerStrengthBonus = 1.23;
+    combatStyleAttackBoost = 0;
+    combatStyleStrengthBoost = 3;
+  //Torture, Primordial, Infernal Cape, Avernic, Ferocious, Ghrazi Rapier
+    equipmentAttackBonus = 161;
+  //Full Torva, Torture, Primordial, Infernal Cape, Avernic, Ferocious, Ultor, Ghrazi Rapier
+    equipmentStrengthBonus = 164;
+    swingTimer = 2.4;
+    battleTime = 0;
+    totalBattleTime = 0;
+    fights = 0;
+    damage = 1;
+    monsterHealth = 330;
 
 //Effective attack level and the resulting max attack roll value
-  effectiveAttackLevel = attackLevel + combatStyleAttackBoost + 8;
-  attackRollMax = effectiveAttackLevel * (equipmentAttackBonus + 64);
+    effectiveAttackLevel = (floor((attackLevel + superCombatBoost) * prayerAttackBonus) + combatStyleAttackBoost + 8);
+    attackRollMax = effectiveAttackLevel * (equipmentAttackBonus + 64);
 //Effective strength and how that translates to damage value
-  effectiveStrengthLevel = strengthLevel + combatStyleStrengthBoost + 8;
-  maximumDamageHit = floor((((effectiveStrengthLevel * (equipmentStrengthBonus + 64)) + 320) / 640));
+    effectiveStrengthLevel = (floor((strengthLevel + superCombatBoost) * prayerStrengthBonus) + combatStyleStrengthBoost + 8);
+    maximumDamageHit = floor((((effectiveStrengthLevel * (equipmentStrengthBonus + 64)) + 320) / 640));
 
 //Monster defensive stats calculation
-  monsterDefenceLevel = 1;
-  monsterDefenceStyleBonus = -15;
+  monsterDefenceLevel = 276;
+  monsterDefenceStyleBonus = 30;
   defenceRollMax = (monsterDefenceLevel + 9) * (monsterDefenceStyleBonus + 64);
 //Checks for attack or defence being higher to determine the combat formula used 
   if (attackRollMax > defenceRollMax)
@@ -63,9 +69,9 @@ int main ()
 
   std::vector<double> trialResults = {};
 
-  while(fights < 5)
+  while(fights < 1000)
   {
-  //While monster is not dead keep hitting it
+    //While monster is not dead keep hitting it
   while (monsterHealth > 0)
   {
     //Random hit roll
@@ -73,36 +79,38 @@ int main ()
     battleTime = battleTime + swingTimer;
     if (hitRoll < hitChanceFixed)
     {
-      std::cout << "Hit \n";
+    //   std::cout << "Hit \n";
       damageRoll = rand();
       //Monkey Patch damage roll
-      if (damageRoll > 16385)
+      if (damageRoll > 585)
       {
-        monsterHealth = monsterHealth - maximumDamageHit;
-          std::cout << maximumDamageHit << " damage dealt \n";
-          std::cout << "Monster Health ";
-          std::cout << monsterHealth;
-          std::cout << "\n";
+        monsterHealth = monsterHealth - (floor((damageRoll / 585)) * 1);
+        //   std::cout << "Monster Health ";
+        //   std::cout << monsterHealth;
+        //   std::cout << "\n";
       }
       else
       {
-        std::cout << "No Damage! \n";
+        // std::cout << "No Damage! \n";
       }
     }
     else
     {
-      std::cout << "Miss \n";
+    //   std::cout << "Miss \n";
     }
   } 
     trialResults.push_back(battleTime);
-    monsterHealth = 5;
+    monsterHealth = 330;
     battleTime = 0;
     fights = fights + 1;
   }
 
-  for (double n : trialResults)
-        std::cout << n << ' ';
-    std::cout << '\n';
+  for (double n : trialResults) {
+    totalBattleTime = totalBattleTime + n;
+  }
+
+  averageTime = (totalBattleTime / trialResults.size());
+    std::cout << averageTime;
   // terminate the program:
   return 0;
 }
